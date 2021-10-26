bb30989c0db3de78d6a04d826971d9c2bdccea88
Alberto Méndez Taboada <amtaboada99@gmail.com>
2021-10-26 13:26:03 +0200

    Engadimos Tarefa 2.3 - Actividade 3

---
+++ Contorno_Servidor/Tema 2/2.3 Elaboración de aplicacións sinxelas en PHP - 2/Actividade_3/T2_3_piramide.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="gl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Título da Páxina</title>
+</head>
+<body>
+    <div style="text-align: center;">
+        <?php
+            echo "* <br>";
+            echo "* * <br>";
+            echo "* * * <br>";
+            echo "* * * * <br>";
+            echo "* * * * * <br>";
+            echo "* * * * * * <br>";
+            echo "* * * * * * * <br>";
+            echo "* * * * * * * * <br>";
+            echo "* * * * * * * * * <br>";
+        ?>
+    </div> 
+</body>
+</html>
+
+
+
