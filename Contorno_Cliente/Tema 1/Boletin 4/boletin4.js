bd53b4d86811b2a8690ce4b73a5112b161941a5b
Alberto Méndez Taboada <amtaboada99@gmail.com>
2021-11-08 14:30:46 +0100

    Creamos Cartafol Boletin 4

---
+++ Contorno_Cliente/Tema 1/Boletin 4/boletin4.js
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////
+//       Boletín 4- Vectores  - Alberto José Méndez Taboada   //    
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+// Exercicio 1: Imprimir os valores dun vector de 100 elementos.
+
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+// Exercicio 2: Lee unha matriz de 5x6, e imprime a 
+// suma total da matriz.
+
+
+///////////////////////////////////////////////////////////////
+
+
+
+
+
+Lee un vector numérico de dimensión 10 e determina cantos elementos son positivos, negativos e ceros, imprimindo por pantalla cada un deles, para finalmente imprimir os contadores por pantalla.
+Ordena ascendentemente unha serie de números dados.
+Dados 10 números, introducilos nun vector e amosar a media e a suma.
+Encher unha matriz 3x3 e amosar a diagonal principal.
+Solicitar os datos dunha matriz 3x4 e amosala trasposta.
+Dado un vector de N elementos, modificar ese vector para que cada elemento conteña a suma dos anteriores.
+Dado un vector de N elementos, devolver outro vector cos elementos ao revés.
+Escribir un programa para ler 10 números por teclado e indicar cantos son maiores que a media.
+
+Crea unha función que reciba un vector bidimensional que contén as posicións dunha partida de xadrez e que pinte o taboleiro correspondente.
