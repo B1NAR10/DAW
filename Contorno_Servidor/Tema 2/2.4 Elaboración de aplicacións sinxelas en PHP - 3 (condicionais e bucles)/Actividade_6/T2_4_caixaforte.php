e72373fb9f3facb6bee216008a943413d7ec082a
Alberto Méndez Taboada <amtaboada99@gmail.com>
2021-11-15 14:09:57 +0100

    Arranxamos a Actividade 6 da Tarefa 2.4

--- Contorno_Servidor/Tema 2/2.4 Elaboración de aplicacións sinxelas en PHP - 3 (condicionais e bucles)/Actividade_6/T2_4_caixaforte.php
+++ Contorno_Servidor/Tema 2/2.4 Elaboración de aplicacións sinxelas en PHP - 3 (condicionais e bucles)/Actividade_6/T2_4_caixaforte.php
@@ -1,7 +1,5 @@
 <?php
         if (isset($_POST['abrir'])){
-
-            
             
             function comprobar(){
                 $combinacion_correcta = 4409;
@@ -11,33 +9,10 @@
                 }else{ 
                     echo "Sintoo, esa non é a combinación"; 
                 }
-            }
-
-//hacer un programa que pida una combinación de 4 dígitos y que compruebe si es correcta o no.
-//si es correcta, debe mostrar un mensaje de que se abre satisfactoriamente.    }       
-        }                             
-
-            // $oportunidades = 0;
-            // while ($oportunidades != 4) {
-            //     $oportunidades++;
-            //     comprobar();   
-            // }
-            
-            $contador = 0;
-            while ($contador <= 4) {
-                comprobar();
-                $contador++;  
-            }
-
-            // for ($i=0; $i < 5; $i++) { 
-               
-                // var color = "" 
-                // while (color != "rojo"){ 
-                // color = prompt("dame un color (escribe rojo para salir)","") 
-    }   
-        //     // }
-        //         echo "Debes esperar un minuto para o volver intentar e inserta a combinación";
-        //    }
+        
+            }                                
+        }    
+           
 ?>
 
 
