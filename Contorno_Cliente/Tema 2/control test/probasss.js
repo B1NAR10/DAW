a4c8a7307169c61311b359aa390b1963f0bcebd6
Alberto Méndez Taboada <amtaboada99@gmail.com>
2021-12-09 16:37:12 +0100

    Cartafol Probas Exame Tipo Test

---
+++ Contorno_Cliente/Tema 2/control test/probasss.js
@@ -0,0 +1,93 @@
+
+function amosar(valor){
+    return Math.max(0, Math.min(10, valor));
+ }function amosar(valor){
+    return Math.max(0, Math.min(10, valor));
+ }
+console.log(amosar(3));
+   
+var saida = "1" + 2 + "3" + 4;
+console.log(saida);
+
+
+var a = false;
+var b = true;
+var valor = a && b;
+console.log(valor);
+
+
+var vector;
+vector = 'true'.split('');
+console.log(vector);
+
+Math.pow(5, 2);
+console.log(Math.pow(5, 2));
+
+var numeros = [1,2,3]
+console.log(numeros);
+
+
+while (condition) {
+    
+}
+console.log(amosar(3));
+   
+var saida = "1" + 2 + "3" + 4;
+console.log(saida);
+
+
+var a = false;
+var b = true;
+var valor = a && b;
+console.log(valor);
+
+
+var vector;
+vector = 'true'.split('');
+console.log(vector);
+function amosar(valor){
+    return Math.max(0, Math.min(10, valor));
+ }
+console.log(amosar(3));
+   
+var saida = "1" + 2 + "3" + 4;
+console.log(saida);
+
+
+var a = false;
+var b = true;
+var valor = a && b;
+console.log(valor);
+
+
+var vector;
+vector = 'true'.split('');
+console.log(vector);
+
+Math.pow(5, 2);
+console.log(Math.pow(5, 2));
+
+var numeros = [1,2,3]
+console.log(numeros);
+
+
+while (condition) {
+    
+}
+Math.pow(5, 2);
+console.log(Math.pow(5, 2));
+
+var numeros = [1,2,3]
+console.log(numeros);
+
+
+    
+
+
+
+substring("cadea principal",-2,-3);
+console.log(substring("cadea principal",-2,-3));
+
+
+numero = 'numero';
+console.log(numero);
