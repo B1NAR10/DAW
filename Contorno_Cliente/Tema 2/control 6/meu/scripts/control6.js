b5b2f47929e1b3812fd2e1b3291af5f33b54e42e
Alberto Méndez Taboada <amtaboada99@gmail.com>
2021-12-10 14:00:00 +0100

    Engado Cartafol Control 6

---
+++ Contorno_Cliente/Tema 2/control 6/meu/scripts/control6.js
@@ -0,0 +1,51 @@
+﻿function xogos() {	
+}
+
+function ranking(campo) {
+}
+
+function activarIconas(icona) {
+}	
+
+function simular() {
+}
+
+function gardar(xogador,xogo,pantallas,puntos) {
+}
+
+function numeroValido(numero) {
+}	
+
+function numeroIncorrecto() {
+}
+
+function imaxes() {
+}
+
+function xogar(url) {
+}
+
+function lista() {
+}
+
+function usuario() {
+}
+
+function usuarioRexistrado() {
+}
+
+function contrasinalIncorrecto() {
+}
+
+function cancelar() {
+}
+
+function sair() {
+}
+
+
+// Inicializa o menú
+function pintarMenu() {
+	texto = '<div id="menu">';
+	menu.innerHTML = texto;
+}
