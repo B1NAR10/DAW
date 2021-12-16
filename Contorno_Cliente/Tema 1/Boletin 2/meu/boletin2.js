9e432981ee9fc97261cf7a7a408c06cb8a259aff
Alberto <amtaboada99@gmail.com>
2021-12-17 00:30:15 +0100

    Engadimos Exercicios 2 (Sen Acabar) e 5

--- Contorno_Cliente/Tema 1/Boletin 2/meu/boletin2.js
+++ Contorno_Cliente/Tema 1/Boletin 2/meu/boletin2.js
@@ -1,4 +1,4 @@
-s////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 //  Boletín 2 - Condicionais    Alberto José Méndez Taboada  //    
 ///////////////////////////////////////////////////////////////
 
@@ -37,26 +37,69 @@ function exercicio2(){
     '<form id = "form1">' +
         '<select id = "figura" onChange = CalcularSuperficieFigura(figura.value)">' +
             '<option selected disabled>Escolle unha Figura</option>' +
-            '<option value = "triángulo">Triángulo</option>' +
-            '<option value = "cadrado">Cadrado</option>' +
-            '<option value = "rectangulo">Rectangulo</option>' +
-            '<option value = "circulo">Círculo</option>' +
+            '<option value = "Triángulo">Triángulo</option>' +
+            '<option value = "Cadrado">Cadrado</option>' +
+            '<option value = "Rectángulo">Rectangulo</option>' +
+            '<option value = "Círculo">Círculo</option>' +
         '</select>'    
     '</form>'
 
+    resultado.innerHTML = '';
+}
+    switch(figura.value){
+        case "triángulo":
+            var base = parseFloat(prompt("Introduza a Base do Triángulo: "));
+            var altura = parseFloat(prompt("Introduza a Altura do Triángulo: "));
+            var superficie = (base * altura) / 2;
+            resultado.innerHTML = "A superficie do Triángulo é: " + superficie;
+            break;
+        case "cadrado":
+            var lado = parseFloat(prompt("Introduza o Lado do Cadrado: "));
+            var superficie = lado * lado;
+            resultado.innerHTML = "A superficie do cadrado é: " + superficie;
+            break;
+        case "rectangulo":
+            var base = parseFloat(prompt("Introduza a Base do Rectángulo: "));
+            var altura = parseFloat(prompt("Introduza a altura do rectangulo: "));
+            var superficie = base * altura;
+            resultado.innerHTML = "A superficie do rectangulo é: " + superficie;
+            break;
+        case "circulo":
+            var radio = parseFloat(prompt("Introduza o Radio do Circulo: "));
+            var superficie = Math.PI * (radio * radio);
+            resultado.innerHTML = "A superficie do circulo é: " + superficie;
+            break;
+        default:
+            resultado.innerHTML = "Non seleccionou ningunha figura";
+    }   
+///////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////
+// Exercicio 2: Dados dous números, indica cal é o maior.
+
+function exercicio5(){
+
+    enunciado.innerHTML = "Dados dous números, indica cal é o maior.";
+    datos.innerHTML = 
+    '<form id = "form1">' +
+        '<p><label>Introduza o Primeiro Número: </label><br><input id="num1"></input></p>' +
+        '<p><label>Introduza o Segundo Número: </label><br><input id="num2"></input></p>' +
+        '<button type ="button" onclick = "CalcularMaior()">Calcular</button>' +
+    '</form>'
+
+    resultado.innerHTML = '';
 }
 
-function CalcularSuperficieFigura(figura){
-    var texto = '';
-    switch(figura){
-        case 'triángulo':
-            datos.innerHTML = "<label for = 'base'>Base: </label><input 
-        case 'cadrado':
-
-        case 'rectangulo':
-
-        case 'circulo':
-
-
+function CalcularMaior(){
+    var numero1 = parseFloat(num1.value);
+    var numero2 = parseFloat(num2.value);
+    
+    if(numero1 > numero2){
+        resultado.innerHTML = "O Número " + numero1 + " é Maior que: " + numero2;
+    }else if(numero1 < numero2){
+        resultado.innerHTML = "O Número " + numero2 + " é Maior que: " + numero1;
+    }else{
+        resultado.innerHTML = "Os números " + numero1 + " e " + numero2 + " son Iguais!";
     }
 }
