e9c4c7ccc0753efd91dfb4b4c1df9123e69c9b69
Alberto <amtaboada99@gmail.com>
2021-12-17 01:24:22 +0100

    Engadimos Exercicios 7, 8, 9, 10, 11(Sen Acabar) e 12

--- Contorno_Cliente/Tema 1/Boletin 2/meu/boletin2.js
+++ Contorno_Cliente/Tema 1/Boletin 2/meu/boletin2.js
@@ -76,7 +76,7 @@ function exercicio2(){
 
 
 ///////////////////////////////////////////////////////////////
-// Exercicio 2: Dados dous números, indica cal é o maior.
+// Exercicio 5: Dados dous números, indica cal é o maior.
 
 function exercicio5(){
 
@@ -101,5 +101,222 @@ function CalcularMaior(){
         resultado.innerHTML = "O Número " + numero2 + " é Maior que: " + numero1;
     }else{
         resultado.innerHTML = "Os números " + numero1 + " e " + numero2 + " son Iguais!";
+    }
+}
+
+///////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////
+// Exercicio 6: Determinar se dous números son iguais ou 
+// diferentes, e se son diferentes indicar cal deles é maior.
+
+
+///////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////
+// Exercicio 7: Dados tres números, indica cal é o maior.
+function exercicio7(){
+
+    enunciado.innerHTML = "Dados tres números, indica cal é o maior.";
+    datos.innerHTML = 
+    '<form id = "form1">' +
+        '<p><label>Introduza o Primeiro Número: </label><br><input id="num1"></input></p>' +
+        '<p><label>Introduza o Segundo Número: </label><br><input id="num2"></input></p>' +
+        '<p><label>Introduza o Terceiro Número: </label><br><input id="num3"></input></p>' +
+        '<button type ="button" onclick = "CalcularMaior()">Calcular</button>' +
+    '</form>'
+
+    resultado.innerHTML = '';
+}
+
+function CalcularMaior(){
+    var numero1 = parseFloat(num1.value);
+    var numero2 = parseFloat(num2.value);
+    var numero3 = parseFloat(num3.value);
+    
+    if(numero1 > numero2 && numero1 > numero3){
+        resultado.innerHTML = "O Número " + numero1 + " é Maior que: " + numero2 + " e " + numero3;
+    }else if(numero2 > numero1 && numero2 > numero3){
+        resultado.innerHTML = "O Número " + numero2 + " é Maior que: " + numero1 + " e " + numero3;
+    }else if(numero3 > numero1 && numero3 > numero2){
+        resultado.innerHTML = "O Número " + numero3 + " é Maior que: " + numero1 + " e " + numero2;
+    }else{
+        resultado.innerHTML = "Os números " + numero1 + ", " + numero2 + " e " + numero3 + " son Iguais!";
+    }
+}
+///////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////
+// Exercicio 8: Pedir un ano e indicar se é ou non bisesto, 
+// tendo en conta que son bisestos todos os múltiplos de 4 
+// agás os múltiplos de 100 (por exemplo o 1900 non foi bisesto), 
+// aínda que os múltiplos de 400 si son bisestos 
+// (por iso o foi o ano 2000).
+
+function exercicio8(){
+    
+    enunciado.innerHTML = "Pedir un ano e indicar se é ou non bisesto, tendo en conta que son bisestos todos os múltiplos de 4 agás os múltiplos de 100 (por exemplo o 1900 non foi bisesto), aínda que os múltiplos de 400 si son bisestos (por iso o foi o ano 2000).";
+    datos.innerHTML = 
+    '<form id = "form1">' +
+        '<p><label>Introduza o Ano: </label><br><input id="num1"></input></p>' +
+        '<button type ="button" onclick = "CalcularBisesto()">Calcular</button>' +
+    '</form>'
+
+    resultado.innerHTML = '';
+}
+
+function CalcularBisesto(){
+    var ano = parseFloat(num1.value);
+    if(ano % 400 == 0 ){
+        resultado.innerHTML = "O Ano " + ano + " é Bisesto!";
+    }else if(ano % 100 == 0){
+        resultado.innerHTML = "O Ano " + ano + " non é Bisesto!";
+    }else if(ano % 4 == 0){
+        resultado.innerHTML = "O Ano " + ano + " é Bisesto!";
+    }else{
+        resultado.innerHTML = "O Ano " + ano + " non é Bisesto!";
+    }
+}
+///////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////
+// Exercicio 9: Pedir un ano e indicar que competición de 
+// seleccións masculinas de balonmán se disputa ese ano, tendo 
+// en conta que os anos múltiplos de 4 celébranse Xogos Olímpicos, 
+// os impares hai Mundiais e os pares Campionatos de Europa.
+
+function exercicio9(){
+        
+    enunciado.innerHTML = "Pedir un ano e indicar que competición de seleccións masculinas de balonmán se disputa ese ano, tendo en conta que os anos múltiplos de 4 celébranse Xogos Olímpicos, os impares hai Mundiais e os pares Campionatos de Europa.";
+    datos.innerHTML = 
+    '<form id = "form1">' +
+        '<p><label>Introduza o Ano: </label><br><input id="num1"></input></p>' +
+        '<button type ="button" onclick = "CalcularCompeticion()">Calcular</button>' +
+    '</form>'
+
+    resultado.innerHTML = '';
+}
+function CalcularCompeticion(){
+    var ano = parseFloat(num1.value);
+    if(ano % 4 == 0){
+        resultado.innerHTML = "O Ano " + ano + " hai Xogos Olímpicos!";
+    }else if(ano % 2 == 0){
+        resultado.innerHTML = "O Ano " + ano + " hai Campionato de Europa!";
+    }else{
+        resultado.innerHTML = "O Ano " + ano + " hai Mundiais!";
+    }
+}
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+// Exercicio 10: Pedir un número do 1 ao 5 e indicar se 
+// é primo ou non.
+
+function exercicio10(){
+    enunciado.innerHTML = "Pedir un número do 1 ao 5 e indicar se é primo ou non.";
+    datos.innerHTML = 
+    '<form id = "form1">' +
+        '<p><label>Introduza un Número: </label><br><input id="num1"></input></p>' +
+        '<button type ="button" onclick = "CalcularPrimo()">Calcular</button>' +
+    '</form>'
+
+    resultado.innerHTML = '';
+}
+
+function CalcularPrimo(){
+    
+    var numero = parseFloat(num1.value);
+    var contador = 0;
+
+    if (numero >= 1 && numero <= 5){
+        
+        for(var i = 1; i <= numero; i++){
+            if(numero % i == 0){
+                contador++;
+            }
+        }
+        if(contador == 2){
+            resultado.innerHTML = "O Número " + numero + " é Primo!";
+        }else{
+            resultado.innerHTML = "O Número " + numero + " non é Primo!";
+        }
+
+    }else{
+        resultado.innerHTML = "O Número " + numero + " non está entre 1 e 5!";
+    }   
+}
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+// Exercicio 11: Pedir un número do 1 ao 7 e amosar 
+// que día da semana é.
+
+function exercicio11(){
+    enunciado.innerHTML = "Pedir un número do 1 ao 7 e amosar que día da semana é.";
+    datos.innerHTML = 
+    '<form id = "form1">' +
+        '<p><label>Introduza un Número: </label><br><input id="num1"></input></p>' +
+        '<button type ="button" onclick = "CalcularDiaSemana()">Calcular</button>' +
+    '</form>'
+
+    resultado.innerHTML = '';
+}
+
+function CalcularDiaSemana(){
+    var numero = parseInt(num1.value);
+    var dia = "";
+    switch(numero){
+        case 1: 
+            dia = "Luns"; 
+            break;
+        case 2: 
+            dia = "Martes"; 
+            break;
+        case 3: 
+            dia = "Mércores";
+            break;
+        case 4: 
+            dia = "Xoves"; 
+            break;
+        case 5: 
+            dia = "Venres"; 
+            break;
+        case 6: 
+            dia = "Sábado"; 
+            break;
+        case 7: 
+            dia = "Domingo"; 
+            break;
+        default: 
+            dia = "Non existe ese día!";
+    }    
+}
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+// Exercicio 12: Pedir unha letra e detectar se é ou non vogal.
+
+function exercicio12(){
+    enunciado.innerHTML = "Pedir unha letra e detectar se é ou non vogal.";
+    datos.innerHTML = 
+    '<form id = "form1">' +
+        '<p><label>Introduza unha Letra: </label><br><input id="letra"></input></p>' +
+        '<button type ="button" onclick = "CalcularVogal(letra.value)">Calcular</button>' +
+    '</form>'
+
+    resultado.innerHTML = '';
+}
+
+function CalcularVogal(letra){
+    var letra = letra.toLowerCase();
+    
+    if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
+        resultado.innerHTML = "A Letra " + letra + " é unha Vogal!";
+    }else{
+        resultado.innerHTML = "A Letra " + letra + " non é unha Vogal!";
     }
 }
