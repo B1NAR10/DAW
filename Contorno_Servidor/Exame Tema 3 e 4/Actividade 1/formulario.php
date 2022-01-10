4006062df8aa4a8489a08a373509973af2063ea5
Alberto Méndez Taboada <amtaboada99@gmail.com>
2022-01-10 14:22:05 +0100

    Engadimos Catafol do Exame Tema 3 e 4

---
+++ Contorno_Servidor/Exame Tema 3 e 4/Actividade 1/formulario.php
@@ -0,0 +1,17 @@
+<?php
+  
+        
+    $usuario1 = $_POST['nome1'];   
+    $usuario2 = $_POST['nome2'];   
+    $contrasinal1 = $_POST['contrasinal1'];   
+    $contrasinal2 = $_POST['contrasinal2'];   
+
+
+    echo "<b>Usuario1:</b><br> Nome: $usuario1<br>";
+    echo "Contrasinal: $contrasinal1<br><br>";
+
+    echo "<b>Usuario2:</b><br> Nome: $usuario2<br>"; 
+    echo "Contrasinal: $contrasinal2<br>";
+
+
+?>
