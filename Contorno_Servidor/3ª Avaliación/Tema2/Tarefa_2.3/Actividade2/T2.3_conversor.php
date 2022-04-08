9ddda40cce2fae5bedf308e3939671a0b253c1a5
Alberto Méndez Taboada <amtaboada99@gmail.com>
2022-04-08 14:23:55 +0200

    Mellora de Codigo Actividade 2 da Tarefa 2.3

--- Contorno_Servidor/3ª Avaliación/Tema2/Tarefa_2.3/Actividade2/T2.3_conversor.php
+++ Contorno_Servidor/3ª Avaliación/Tema2/Tarefa_2.3/Actividade2/T2.3_conversor.php
@@ -1,25 +1,56 @@
 <?php
 
-    $divisa = $_POST['divisa'];
-    $tipo_moeda = $_POST['moeda'];
+    $divisa = $_POST['divisa']; //Valor en nº 
+    $tipo_moeda = $_POST['moeda']; //Tipo de moeda
 
-    print "<h1>Conversor de Moedas</h1>";
+    $dolar = 1.0884;
+    $euro  = 0.90884;
+    $peseta = 166.386;
+
     
-    function ConversorEuroDolar($divisa, $tipo_moeda) {
-        if ($tipo_moeda == '$') {  $resultado = round($divisa / 1.0884, 2);
-                                        
-        }elseif ($tipo_moeda == '€') {  $resultado = round($divisa * 1.0884, 2);
-                                         
-          return $resultado; 
-            }
-    }
+    echo "<div style=\"text-align:center\">";
+        print("<h1>Conversor de divisas</h1>");
+    echo "</div>";
 
-    function ConversorEuroPeseta($divisa, $tipo_moeda) {
-        if ($tipo_moeda == '$') {  $resultado = round($divisa * 166.386, 2);                                    
-        }elseif ($tipo_moeda == '€') {  $resultado = round($divisa / 166.386, 2);                                 
-       return $resultado;
-        }  
-    }
+    echo "<div style=\"color:orange\">";
 
+        switch ($tipo_moeda) {
+            case "$":
+                $euro = round($divisa * $dolar, 2);
+                $peseta = round($euro * 166.386, 2);
+                echo $divisa." $ equivalen a: </br> </br>";
+                echo "Euros: ".$euro." € <br>";
+                echo "Pesetas: ".$peseta." Pts";
+                break;
+            case "€":
+                $dolar = round($divisa * $euro, 2);
+                $peseta = round($divisa * $peseta, 3);
+
+                echo $divisa." € equivalen a: <br></br>";
+                echo "Dolar: ".$dolar." $ <br>";
+                echo "Pesetas: ".$peseta." Pts";
+                break;
+            case "p":
+                $resultado = $divisa * $peseta;
+                print("<p>$divisa $tipo_moeda = $resultado $tipo_moeda</p>");
+                break;
+            default:
+                print("<p>Erro</p>");
+                break;
+        }
+    echo "</div>";
+
+
+    $euro_dolar   = round($divisa / 0.858704, 2);
+    $dolar_euro   = round($divisa * 0.858704, 2);
+
+    $euro_peseta  = round($divisa * 166.386, 2);
+    $peseta_euro  = round($divisa / 166.386, 2);
+
+    $dolar_peseta = round($dolar_euro * 166.386, 2);
+    $peseta_dolar = round($euro_dolar / 166.386, 2);
+
+  
+    
 
 ?>
