f1109f2b1ad904ac5363637434664fb2e4169c27
Alberto <amtaboada99@gmail.com>
2022-04-11 14:26:45 +0200

    Engadimos Actividade 2 da Tarefa 2.3

--- Contorno_Servidor/3ª Avaliación/Tema2/Tarefa_2.3/Actividade2/T2.3_conversor.php
+++ Contorno_Servidor/3ª Avaliación/Tema2/Tarefa_2.3/Actividade2/T2.3_conversor.php
@@ -4,53 +4,38 @@
     $tipo_moeda = $_POST['moeda']; //Tipo de moeda
 
     $dolar = 1.0884;
-    $euro  = 0.90884;
     $peseta = 166.386;
 
-    
     echo "<div style=\"text-align:center\">";
         print("<h1>Conversor de divisas</h1>");
-    echo "</div>";
-
-    echo "<div style=\"color:orange\">";
-
         switch ($tipo_moeda) {
             case "$":
-                $euro = round($divisa * $dolar, 2);
-                $peseta = round($euro * 166.386, 2);
+                $euro = round($divisa / $dolar, 2);
+                $peseta = round($divisa * $peseta, 2);
                 echo $divisa." $ equivalen a: </br> </br>";
                 echo "Euros: ".$euro." € <br>";
                 echo "Pesetas: ".$peseta." Pts";
                 break;
+                
             case "€":
                 $dolar = round($divisa * $euro, 2);
                 $peseta = round($divisa * $peseta, 3);
-
                 echo $divisa." € equivalen a: <br></br>";
                 echo "Dolar: ".$dolar." $ <br>";
                 echo "Pesetas: ".$peseta." Pts";
                 break;
+
             case "p":
-                $resultado = $divisa * $peseta;
-                print("<p>$divisa $tipo_moeda = $resultado $tipo_moeda</p>");
+                $euro  = round($divisa / $peseta, 3);
+                $dolar = round($euro * $dolar, 3);
+                echo $divisa." Pts equivalen a: <br></br>";
+                echo "Euros: ".$euro." € <br>";
+                echo "Dolar: ".$dolar." $ <br>";
                 break;
+
             default:
                 print("<p>Erro</p>");
                 break;
         }
     echo "</div>";
-
-
-    $euro_dolar   = round($divisa / 0.858704, 2);
-    $dolar_euro   = round($divisa * 0.858704, 2);
-
-    $euro_peseta  = round($divisa * 166.386, 2);
-    $peseta_euro  = round($divisa / 166.386, 2);
-
-    $dolar_peseta = round($dolar_euro * 166.386, 2);
-    $peseta_dolar = round($euro_dolar / 166.386, 2);
-
-  
-    
-
 ?>
