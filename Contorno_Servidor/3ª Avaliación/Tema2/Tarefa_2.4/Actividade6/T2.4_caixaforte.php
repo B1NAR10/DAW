771232411cb0d0e6b7955460f6e9c0dd10c58600
Alberto <amtaboada99@gmail.com>
2022-04-11 23:35:13 +0200

    Corriximos Erro na Actividade 6 da Tarefa 2.4

--- Contorno_Servidor/3ª Avaliación/Tema2/Tarefa_2.4/Actividade6/T2.4_caixaforte.php
+++ Contorno_Servidor/3ª Avaliación/Tema2/Tarefa_2.4/Actividade6/T2.4_caixaforte.php
@@ -1,16 +1,14 @@
 <?php
         if (isset($_POST['abrir'])){
             
-            function comprobar(){
                 $combinacion_correcta = 4409;
                 $combinacion = $_POST['combinacion'];
+                
                 if ($combinacion == $combinacion_correcta) { 
                     echo "A caixa forte abréuse satisfactoriamente"; 
                 }else{ 
                     echo "Sintoo, esa non é a combinación"; 
-                }
-        
-            }                                
+                }                                
         }    
            
 ?>
