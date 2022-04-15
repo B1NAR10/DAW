0e8b62c794944e658082813fe9878b44cfe0bbe6
Alberto <amtaboada99@gmail.com>
2022-04-15 17:35:38 +0200

    Engadimos Solución Actividade 1 da Tarefa 2.5

--- Contorno_Servidor/3ª Avaliación/Tema2/Tarefa_2.5/Actividade_1/T2.5_ordear_numeros.php
+++ Contorno_Servidor/3ª Avaliación/Tema2/Tarefa_2.5/Actividade_1/T2.5_ordear_numeros.php
@@ -1,15 +1,25 @@
 <?php
 
-    if (isset($_POST['calcular'])) {
-        $numeros = array();
-        $numeros = $_POST['numeros'];   
-        $numeros = array_filter($numeros); // array_filter: elimina elementos de un array que non satisfagan unha condición.
-        $numeros = array_values($numeros); // array_values: devolve un array con valores únicos.
-        $numeros = array_unique($numeros); // array_unique: elimina elementos duplicados dun array.
-        $numeros = array_reverse($numeros); // array_reverse: inverte a orde de elementos dun array.
-        $numeros = array_slice($numeros, 0, 3); // array_slice: devolve un subconxunto de elementos dun array.
-        $numeros = implode(", ", $numeros); // implode: devolve unha string formada por elementos dun array.    
-        echo "Os numeros ordenados son: " . $numeros;
-    }
+    $n1 =  $_POST['n1'];
+    $n2 =  $_POST['n2'];
+    $n3 =  $_POST['n3'];
+    $n4 =  $_POST['n4'];
+    $n5 =  $_POST['n5'];
+    $n6 =  $_POST['n6'];
+    $n7 =  $_POST['n7'];
+    $n8 =  $_POST['n8'];
+    $n9 =  $_POST['n9'];
+    $n10 = $_POST['n10'];
 
+    $numeros = array($n1, $n2, $n3, $n4, $n5, $n6, $n7, $n8, $n9, $n10);
+    rsort($numeros);
+
+    echo '<h1>Tarefa 2.5 Elaboración de aplicacións sinxelas en PHP - 4 (Arrays e Funcións)</h1>';
+    echo '<h2>Actividade 1 - Array de 10 números ordeado de Maior a Menor </h2> <br>';
+    echo 'Os números ordeados son: ';
+    foreach ($numeros as $numero) {
+        echo $numero.' ';
+
+    }
+  
 ?>
