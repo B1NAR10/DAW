ea3bc1c0674b7fee078476ad6946626ae89638c4
Alberto <amtaboada99@gmail.com>
2022-04-19 05:36:01 +0200

    Engadimos Solución Actividade 3 da Tarefa 2.5

---
+++ Contorno_Servidor/3ª Avaliación/Tema2/Tarefa_2.5/Actividade_5/T2.5_primos.php
@@ -0,0 +1,13 @@
+<?php
+
+// foreach($enteiros as $enteiro){
+        //     //echo ePrimo($enteiro) . "<br>";
+        //     if (ePrimo($enteiro) === $enteiros) {
+        //         echo '<span style="color:red">' . ePrimo($enteiro). '</span>';
+        //     } else {
+        //         echo '<span style="color:blue">' . ePrimo($enteiro) . '</span>';
+        //     }
+        // }
+
+
+?>
