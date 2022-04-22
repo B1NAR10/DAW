3b65023cc672037cb3d3f276bcdc0a7f2206bcfb
Alberto <amtaboada99@gmail.com>
2022-04-22 19:42:50 +0200

    Engadimos Correción Estética da Actividade 2 da Tarefa 3.1

--- Contorno_Servidor/3ª Avaliación/Tema3/Tarefa_3.1/Actividade2/sumar.php
+++ Contorno_Servidor/3ª Avaliación/Tema3/Tarefa_3.1/Actividade2/sumar.php
@@ -8,6 +8,6 @@
     echo "<h1>Tarefa 3.1 Tarefa 3.1 GET, POST e Cookies</h1>";
     echo "<h2>Actividade 2 - Sumar 4 Números e envialos por GET </h2> <br>";
 
-    echo "<b>Sumamos os 4 Números: </b>" . ($n1 + $n2 + $n3 + $n4) . "<br><br>";
+    echo "<b>Sumamos os Números: " .$n1. " + ".$n2." + ".$n3." + ".$n4. " = </b>" . ($n1 + $n2 + $n3 + $n4) . "<br><br>";
 
 ?>
