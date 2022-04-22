699ae1712eb328ffff23d66a83031679898bcb22
Alberto <amtaboada99@gmail.com>
2022-04-22 20:01:29 +0200

    Engadimos Actividade 3 da Tarefa 3.1 Rematada

---
+++ Contorno_Servidor/3ª Avaliación/Tema3/Tarefa_3.1/Actividade3/sumar.php
@@ -0,0 +1,13 @@
+<?php
+
+    $n1 = $_POST['n1'];
+    $n2 = $_POST['n2'];
+    $n3 = $_POST['n3'];
+    $n4 = $_POST['n4'];
+
+    echo "<h1>Tarefa 3.1 Tarefa 3.1 GET, POST e Cookies</h1>";
+    echo "<h2>Actividade 3 - Sumar 4 Números e envialos por POST </h2> <br>";
+
+    echo "<b>Sumamos os Números: " .$n1. " + ".$n2." + ".$n3." + ".$n4. " = </b>" . ($n1 + $n2 + $n3 + $n4) . "<br><br>";
+
+?>
