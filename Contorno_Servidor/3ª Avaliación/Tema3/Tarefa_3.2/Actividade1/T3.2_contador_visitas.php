977ed9bfaebac324977c9160e65c3438b747dc08
Alberto <amtaboada99@gmail.com>
2022-04-23 01:25:15 +0200

    Engadimos Actividade 1 da Tarefa 3.2 Rematada

--- Contorno_Servidor/3ª Avaliación/Tema3/Tarefa_3.2/Actividade1/T3.2_contador_visitas.php
+++ Contorno_Servidor/3ª Avaliación/Tema3/Tarefa_3.2/Actividade1/T3.2_contador_visitas.php
@@ -9,7 +9,19 @@
     <h1>Tarefa 3.2  Sesións</h1>
     <h2>Actividade 1 - Contador de Visitas con Sesións </h2> <br>
 
-    
+    <?php
+
+        session_start();
+
+        if(!isset($_SESSION['contador'])){
+            $_SESSION['contador'] = 0;
+        }
+
+        $_SESSION['contador']++;
+
+        echo "<b>Visitas: </b>" . $_SESSION['contador'];
+
+    ?>
 		
 	</form>   
     
