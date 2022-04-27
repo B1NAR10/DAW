abefceea4b459fed6f199e43eca2f9ac759b6d6e
Alberto <amtaboada99@gmail.com>
2022-04-27 16:28:31 +0200

    Cambiamos Extensión do Arquivo da Actividade 2 da Tarefa 3.2

---
+++ Contorno_Servidor/3ª Avaliación/Tema3/Tarefa_3.2/Actividade2/T3.2_media_numeros.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="gl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarefa 3.2 Actividade 2</title>
+</head>
+<body>
+    <h1>Tarefa 3.2 Sesións</h1>
+    <h2>Actividade 2 - Calcular Media con Sesións </h2> <br>
+
+   
+
+    <?php
+
+        
+       
+        // Recoger datos teclado con sesiones
+        session_start();
+        
+
+        // for(int $i = 0; $i < 0; $i++) {
+        //     $n1[$i] = $_POST['numeros'];
+        // }
+        
+        for ($i=0; $i<0; $i++) {
+
+            echo "<br>Entrada ".($i+1)."<input type='text' name='input".($i+1)."'>";
+        
+        }
+        
+
+    ?>
+    
+</body>
+</html>
