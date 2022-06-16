bb005cff5d23e67a4e9e9abcf419df73e83fb01a
Alberto <amtaboada99@gmail.com>
2022-06-16 17:28:45 +0200

    Eliminamos Cartafol Solucións, esta nos Temas

--- Contorno_Cliente/3ª Avaliación/solucions/solución control 3/scripts/funcions.js
+++
@@ -1,7 +0,0 @@
-function moverImaxe(imaxe) {
-	setTimeout(function () { 
-		posicion += 0.5;
-		caminante.style.marginLeft = posicion + 'em';
-		caminante.src = './imaxes/caminante/' + imaxe + '.png';
-	}, 250 * imaxe);
-}
